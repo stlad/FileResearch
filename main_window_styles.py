fcc9fd33101674c930efbbd3f64a8a19b8e69ea4
stlad <82213506+stlad@users.noreply.github.com>
2022-07-11 13:03:36 +0500

    добавлены стили, кнопки назад и вперед из папок

---
+++ main_window_styles.py
@@ -0,0 +1,68 @@
+
+
+main_window_style = '\n'.join(['background-color:#010812'])
+colors = '010812 0b212f 44586a a2b4c3 cdd4db'
+
+
+file_button_style ='\n'.join([
+'QPushButton{',
+'background-color:#44586a;',
+'border:5px solid #44586a;',
+'border-radius: 10px;',
+'color:#a2b4c3;',
+'font-size:15px;',
+'padding:5px 5px 5px 5px',
+'}',
+
+'QPushButton:hover{',
+'border:2px solid #a2b4c3;',
+'}',
+
+'QPushButton:hover:pressed{',
+'background-color:#0b212f;',
+'}'
+])
+
+
+text_browser_style = '\n'.join([
+'QTextBrowser{',
+'background-color: #44586a;',
+'font-size:12px;',
+'color: #a2b4c3;',
+'},'
+])
+
+browse_button_style ='\n'.join([
+'QPushButton{',
+'background-color:#44586a;',
+'color:#a2b4c3;',
+'font-size:15px;',
+'}'])
+
+path_text_style = '\n'.join([
+'QLineEdit{',
+'background-color:#44586a;',
+'color:#a2b4c3;',
+'font-size:12px;',
+"padding:5px 5px 5px 5px;",
+'}'])
+
+next_dir_button_style='\n'.join([
+'QPushButton{',
+'background-color:#44586a;',
+'border:5px solid #44586a;',
+'border-radius: 10px;',
+'color:#a2b4c3;',
+'font-size:15px;',
+'padding:10px 10px 10px 10px',
+'}',
+
+'QPushButton:hover{',
+'border:5px solid #0b212f;',
+'}',
+
+'QPushButton:hover:pressed{',
+'background-color:#0b212f;',
+'}'
+])
+
